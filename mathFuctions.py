e2adb95e5eb5976c9eb1dea10b37261e7cadec46
Trent Boatright <trent.aa.boatright@gmail.com>
2021-04-14 17:40:53 -0500

    Create mathFunctions.py
    Created mathFunctions file. Contains Addition, Subtraction, Multiplication, and Division functions. More can be added later, and this file is meant to be imported to other files that need its functions.

---
+++ mathFuctions.py
@@ -0,0 +1,19 @@
+#Addition
+def add(a = 1, b = 1):
+    c = a + b
+    return c
+
+#Subtraction
+def subtract(a = 1, b = 1):
+    c = a - b
+    return c
+
+#Multiplacation
+def multiply(a = 1, b = 1):
+    c = a * b
+    return c
+
+#Division
+def divide(a = 1, b = 1):
+    c = a / b
+    return c
